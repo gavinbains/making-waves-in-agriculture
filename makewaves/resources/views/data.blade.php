54853773df37d7739627a552c934e58607ad9025
yiuj <yiuj@usc.edu>
2018-02-24 16:17:43 -0700

    some stuff

--- makewaves/resources/views/data.blade.php
+++ makewaves/resources/views/data.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Making Waves
+                    This is your data
                 </div>
 
                 <div class="links">
